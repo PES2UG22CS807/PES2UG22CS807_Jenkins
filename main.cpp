9dc92871ab0c1c02a8b754b72fe3972b703e58e6
PES2UG22CS807 <162550612+PES2UG22CS807@users.noreply.github.com>
2024-03-12 22:01:14 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "This is PES2UG22CS807 \n";
+  cout << "This is CPP file\n";
+  cout << "I have successfully built and run\n";
+
+  return 0;
+}
